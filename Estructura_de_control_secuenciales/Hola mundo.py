c7d07b9a74b4ba55cd35502f5e876b524c51c5de
MiguelAngel2038 <114430984+MiguelAngel2038@users.noreply.github.com>
2022-10-03 12:31:41 -0500

    Python

---
+++ Estructura_de_control_secuenciales/Hola mundo.py
@@ -0,0 +1,14 @@
+print("!Hola mundo¡")
+
+# Este es un comentario de una sola linea
+"""
+Este es un comentario de varias lineas
+"""
+#Entrada
+edad_1=int(input("Ingrese la primera edad: "))
+edad_2=int(input("Ingrese la segunda edad: "))
+edad_3=int(input("Ingrese la tercera edad: "))
+# Caja negra
+promedio=(edad_1+edad_2+edad_3)/3
+#Salida
+print("Este es el promedio:", promedio)
