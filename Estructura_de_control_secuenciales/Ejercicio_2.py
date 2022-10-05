854e179e85930665d6bb13e3ab91073a87b8ebcd
Miguel <114430984+MiguelAngel2038@users.noreply.github.com>
2022-10-04 21:31:02 -0500

    Python

---
+++ Estructura_de_control_secuenciales/Ejercicio_2.py
@@ -0,0 +1,7 @@
+#Entrada
+capital= int (input("Ingrese su capital: "))
+#Caja negra
+interes= float (capital*0.02)
+total= float (interes+capital)
+#Salida
+print("El total que ganará será de: ", total, "con intereses obtenidos de:", interes)
